55561bcda41edaa87be317a33a6c326cbb8d4ffe
MinGyeongTae <mgt3835@naver.com>
2015-11-26 00:26:07 +0900

    0.1
    이미지로드

---
+++ 기말팀프0.1/기말팀프0.1/기말팀프0.1.py
@@ -0,0 +1,22 @@
+﻿import pygame
+
+def Main():
+    pygame.init()
+    screen = pygame.display.set_mode((640, 400))
+    duck = pygame.image.load("ozil.jpg")
+    duck_rect = duck.get_rect()
+    duck_rect = duck_rect.move((10, 10))
+
+    while True:
+        for event in pygame.event.get():
+            if pygame.KEYDOWN == event.type:
+                if pygame.K_ESCAPE == event.key:
+                    return
+            if pygame.QUIT == event.type:
+                return
+
+        screen.fill((0, 0, 0))
+        screen.blit(duck, duck_rect)
+        pygame.display.flip()
+
+Main()
